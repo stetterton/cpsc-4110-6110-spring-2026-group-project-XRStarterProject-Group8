5b3c47bfc3ae629a4d2a08e6260f2695c1e690cb
WillLaWill <64604088+WillLaWill@users.noreply.github.com>
2026-04-24 17:32:01 -0400

    Script Updates

--- Assets/_Project/Scripts/Runtime/SpawnGeneric.cs
+++ Assets/_Project/Scripts/Runtime/SpawnGeneric.cs
@@ -5,14 +5,23 @@
 //  Created by William Harris on 3/1/2026.
 //  May god have mercy on my lactic acids
 
+using Unity.Mathematics;
 using UnityEngine;
 
 public class SpawnGeneric : MonoBehaviour
 {
     public GameObject prefab;
-    public Transform position;
+    public GameObject positionFinder;
+    Vector3 spawnPosition;
+    quaternion spawnRotation;
+
+    private void Update()
+    {
+        spawnPosition = positionFinder.transform.position;
+        spawnRotation = positionFinder.transform.rotation;
+    }
     public void Spawn()
     {
-        Instantiate(prefab, position);
+        Instantiate(prefab, spawnPosition, spawnRotation);
     }
 }
